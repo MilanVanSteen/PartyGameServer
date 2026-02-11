aea24140d43fe5d7d5fd9900717905fb5de5fc25
MilanVanSteen <537674@student.fontys.nl>
2026-02-11 14:06:54 +0100

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { createRoom, joinRoom, leaveRoom, getRoomPlayers } = require("./rooms/roommanager");
+const { createRoom, joinRoom, leaveRoom, setPlayerName, getRoomPlayers } = require("./rooms/roommanager");
 
 const hosts = {};
 
@@ -24,33 +24,28 @@ io.on("connection", (socket) => {
     // JOIN_ROOM
     socket.on("JOIN_ROOM", ({ roomCode }) => {
         const success = joinRoom(roomCode, socket.id);
-        if (success) {
-            socket.join(roomCode);
-            socket.roomCode = roomCode;
-            socket.emit("ROOM_JOINED", { roomCode, players: getRoomPlayers(roomCode), host: hosts[roomCode] });
-            io.to(roomCode).emit("PLAYER_JOINED", { playerId: socket.id, players: getRoomPlayers(roomCode), roomCode });
-           console.log(`${socket.id} joined room ${roomCode}`);
-        } else {
+
+        if (!success) {
             socket.emit("ERROR", { message: "Room does not exist" });
+            return;
         }
+        
+        socket.join(roomCode);
+        socket.roomCode = roomCode;
+
+        socket.emit("ROOM_JOINED", { roomCode, players: getRoomPlayers(roomCode)});
+        
+        io.to(roomCode).emit("PLAYER_JOINED", {players: getRoomPlayers(roomCode)});
     });
 
     // SET_NAME
     socket.on("SET_NAME", ({ playerName }) => {
-        const room = rooms[socket.roomCode];
+        const roomCode = socket.roomCode;
         if (!roomCode) return socket.emit("ERROR", { message: "Not in a room" });
+
+        setPlayerName(roomCode, socket.id, playerName);
         
-        const players = getRoomPlayers(roomCode);
-        let player = players.find(p => p.id === socket.id);
-        if (player) {
-            player.name = playerName; // set the name
-        } else {
-            // if not found, add
-            players.push({ id: socket.id, name: playerName });
-        }
-        
-        io.to(roomCode).emit("PLAYER_JOINED", { players });
-        console.log(`Player ${socket.id} set name to ${playerName} in room ${roomCode}`);
+        io.to(roomCode).emit("PLAYER_JOINED", {players: getRoomPlayers(roomCode)});
     });
 
     // HOST starts game
--- rooms/roommanager.js
+++ rooms/roommanager.js
@@ -2,24 +2,41 @@ const rooms = {};
 
 function createRoom(socketId) {
   const roomCode = Math.random().toString(36).substring(2, 6).toUpperCase();
-  rooms[roomCode] = { players: [socketId] };
+  rooms[roomCode] = {
+    players: [{ id: socketId, name: null }]
+  };
   return roomCode;
 }
 
 function joinRoom(roomCode, socketId) {
   if (!rooms[roomCode]) return false;
-  rooms[roomCode].players.push(socketId);
+
+  rooms[roomCode].players.push({
+    id: socketId,
+    name: null
+  });
+
   return true;
 }
 
 function leaveRoom(roomCode, socketId) {
   if (!rooms[roomCode]) return;
-  rooms[roomCode].players = rooms[roomCode].players.filter(id => id !== socketId);
+
+  rooms[roomCode].players = rooms[roomCode].players.filter(p => p.id !== socketId);
+
   if (rooms[roomCode].players.length === 0) delete rooms[roomCode];
+}
+
+function setPlayerName(roomCode, socketId, name) {
+  const room = rooms[roomCode];
+  if (!room) return;
+
+  const player = room.players.find(p => p.id === socketId);
+  if (player) player.name = name;
 }
 
 function getRoomPlayers(roomCode) {
   return rooms[roomCode] ? rooms[roomCode].players : [];
 }
 
-module.exports = { createRoom, joinRoom, leaveRoom, getRoomPlayers };
+module.exports = { createRoom, joinRoom, leaveRoom, setPlayerName, getRoomPlayers };
