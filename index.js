6afea4d550ce836033e4ae25c190f612661a173f
MilanVanSteen <537674@student.fontys.nl>
2026-06-10 10:57:29 +0200

    Reusing created room

--- index.js
+++ index.js
@@ -21,11 +21,18 @@ io.on("connection", (socket) => {
 
     // CREATE_ROOM
     socket.on("CREATE_ROOM", () => {
+        const existing = Object.entries(hosts)
+            .find(([room, hostId]) => hostId === socket.id);
+
+        if (existing) {
+            socket.emit("ROOM_CREATED", { roomCode: existing[0] });
+            return;
+        }
+        
         const roomCode = createRoom(socket.id);
         socket.join(roomCode);
 
         hosts[roomCode] = socket.id;
-
         socket.roomCode = roomCode;
         
         socket.emit("ROOM_CREATED", { roomCode });
